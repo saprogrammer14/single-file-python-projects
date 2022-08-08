e9d34b841ff249edae42114d7734de3be643d0d8
saprogrammer14 <shoumik.khowai@gmail.com>
2022-08-08 09:27:22 +0530

    fixed some bugs

--- rock paper scissor(console game).py
+++ rock paper scissor(console game).py
@@ -1,54 +1,53 @@
+# rock paper scissor game
+# by: Shoumik Acharya
+# Human v/s Computer
+
 import random
 
-human = ""
-computer = ""
-# check if human is the scorer
-def scorer():
+# check the scorer
+# r for rock, p for paper, s for scissor
+def scorer(h, c):
     # r>s, p>r, s>p
-    if (
-        (human == "r" and computer == "s")
-        or (human == "p" and computer == "r")
-        or (human == "s" and computer == "p")
-    ):
-        return True
-    elif human==computer:
+    if (h == "r" and c == "s") or (h == "p" and c == "r") or (h == "s" and c == "p"):
+        return "h"
+    elif h == c:
         return "tie"
-    else:
-        return False
+    elif (c == "r" and h == "s") or (c == "p" and h == "r") or (c == "s" and h == "p"):
+        return "c"
 
-
-
-print(scorer())
 
 def play(high_score):
     human_score = 0
     computer_score = 0
 
     while human_score != high_score and computer_score != high_score:
-        human = input("'r' for rock, 'p' for paper, 's' for scissor: ")
-        computer = random.choice(['r','p','s'])
+        human = input("'r' for rock, 'p' for paper, 's' for scissor: ")  # Human input
+        computer = random.choice(["r", "p", "s"])  # computer input
 
-        if scorer():
-            human_score += 1
-            print("HUMAN!")
+        if scorer(human, computer) == "h":
+            print("YOU!")
             print(f"computer - {computer}")
-            print(f"human = {human_score}, computer = {computer_score}")
-        
-        elif scorer()=="tie":
+            human_score += 1
+            print(f"your score - {human_score}, computer score - {computer_score}")
+            print("\n")
+
+        elif scorer(human, computer) == "c":
+            print("COMPUTER!")
+            print(f"computer - {computer}")
+            computer_score += 1
+            print(f"your score - {human_score}, computer score - {computer_score}")
+            print("\n")
+
+        elif scorer(human, computer) == "tie":
             print("tie")
             print(f"computer - {computer}")
-        else:
-            computer_score += 1
-            print(f"COMPUTER!")
-            print(f"computer - {computer}")
-            print(f"human = {human_score}, computer = {computer_score}")
+            print(f"your score - {human_score}, computer score - {computer_score}")
+            print("\n")
 
     if human_score == high_score:
-        print("Human won! ")
+        print("You won!")
     else:
-        print("Computer won! ")
-
-    print(f"final score: human = {human_score}, computer = {computer_score}")
+        print("Computer won!")
 
 
-play(5)
+play(7)
